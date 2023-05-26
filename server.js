8201378c1a43cf68287ed78c2681a01e0daf8fbb
Jsrra <josephserrato10@gmail.com>
2023-05-26 03:17:40 -0500

    server update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const routes = require('./routes');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,14 +10,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
+
+mongoose.set("debug", true);
+
+app.use(require(routes));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+
+
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network-api", {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
 // });
-
-mongoose.set("debug", true);
-
-app.use(require('./routes'));
-
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
